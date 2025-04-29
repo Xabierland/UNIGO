46af027f8c5cc5dd2a0cf5aa666519069ad30312
Eneko Etxaniz Monge <eenneekkooeemm@gmail.com>
2025-04-29 20:26:05 +0200

    Guardar fragmento actual al rotar la pantalla. También se ha establecido la estructura para abrir los fragmentos

--- src/app/src/main/java/com/ehunzango/unigo/activities/MainActivity.java
+++ src/app/src/main/java/com/ehunzango/unigo/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ehunzango.unigo.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -21,8 +23,28 @@ public class MainActivity extends BaseActivity {
     private BottomNavigationView bottomNav;
     private FirebaseAuthService authService;
 
+    // Aqui se guarda el nombre del fragmento actual, así cuando se recarge la actividad (al rotar la pantalla) no se pierde
+    private FragmentType actualFragment = FragmentType.MAP;
+
+    // Si usamos un enum es más facil referenciarlos
+    public enum FragmentType
+    {
+        // enum -> int : enum.ordinal()
+        // int -> enum : FragmentType.values()[int]
+        MAP,
+        PROFILE,
+        SETTINGS
+    }
+
+    //              +--------------------------------------------------------------------------+
+    //              |                                                                          |
+    //              |                             INICIALIZACION                               |
+    //              |                                                                          |
+    //              +--------------------------------------------------------------------------+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
@@ -31,9 +53,18 @@ public class MainActivity extends BaseActivity {
         
         // Inicializar servicios
         authService = FirebaseAuthService.getInstance();
+
+        // Obtener los datos del saved instance
+        if(savedInstanceState != null)
+        {
+            actualFragment = FragmentType.values()[savedInstanceState.getInt("actualFragment")];
+        }
         
         // Configurar navegación
         setupNavigation();
+
+        // Abrir fragmento actual
+        openCurrentFragment();
     }
 
     @Override
@@ -41,36 +72,84 @@ public class MainActivity extends BaseActivity {
         bottomNav = findViewById(R.id.bottom_navigation);
     }
     
-    private void setupNavigation() {
+    private void setupNavigation()
+    {
+        // Establecer ítem seleccionado
+        switch (actualFragment)
+        {
+            case PROFILE:
+                bottomNav.setSelectedItemId(R.id.nav_profile);
+                break;
+
+            case SETTINGS:
+                bottomNav.setSelectedItemId(R.id.nav_settings);
+                break;
+
+            case MAP:
+            default:
+                bottomNav.setSelectedItemId(R.id.nav_map);
+                break;
+        }
+
         // Configurar listener para la navegación inferior
+        // Primero se selecciona el item correspondiente, para que no salte el listener y carge el fragmento dos veces
         bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int itemId = item.getItemId();
 
                 // Navegar al fragmento correspondiente basado en el ítem seleccionado
-                if (itemId == R.id.nav_map) {
+                if (itemId == R.id.nav_map)
+                {
                     // Cargar fragmento de mapa
-                    showToast("Mapa - Por implementar");
+                    openMapFragment();
                     return true;
-                } else if (itemId == R.id.nav_profile) {
+                }
+                else if (itemId == R.id.nav_profile)
+                {
                     // Cargar fragmento de perfil
-                    showToast("Perfil - Por implementar");
+                    openProfileFragment();
                     return true;
-                } else if (itemId == R.id.nav_settings) {
+                }
+                else if (itemId == R.id.nav_settings)
+                {
                     // Cargar fragmento de ajustes
-                    showToast("Ajustes - Por implementar");
+                    openSettingsFragment();
                     return true;
                 }
 
                 return false;
             }
         });
-        
-        // Establecer ítem por defecto
-        bottomNav.setSelectedItemId(R.id.nav_map);
     }
-    
+
+    private void openCurrentFragment()
+    {
+        switch (actualFragment)
+        {
+            case PROFILE:
+                openProfileFragment();
+                break;
+
+            case SETTINGS:
+                openSettingsFragment();
+                break;
+
+            case MAP:
+            default:
+                openMapFragment();
+                break;
+        }
+    }
+
+
+
+    //              +--------------------------------------------------------------------------+
+    //              |                                                                          |
+    //              |                      GESTIÓN DE SESION DE USUARIO                        |
+    //              |                                                                          |
+    //              +--------------------------------------------------------------------------+
+
     /**
      * Método para cerrar la sesión del usuario.
      * Se puede llamar desde los fragmentos si es necesario.
@@ -91,11 +170,86 @@ public class MainActivity extends BaseActivity {
                 .setNegativeButton(R.string.dialog_button_cancel, null)
                 .show();
     }
-    
+
+
+    //              +--------------------------------------------------------------------------+
+    //              |                                                                          |
+    //              |                            RECUPERACION DE DATOS                         |
+    //              |                                                                          |
+    //              +--------------------------------------------------------------------------+
+
+    public void onSaveInstanceState(Bundle bundle)
+    {
+        super.onSaveInstanceState(bundle);
+
+        bundle.putInt("actualFragment", actualFragment.ordinal());
+    }
+
+    public void onRestoreInstanceState (Bundle bundle)
+    {
+
+    }
+
+    //              +--------------------------------------------------------------------------+
+    //              |                                                                          |
+    //              |                     APERTURA Y GESTIÓN DE ACTIVIDADES                    |
+    //              |                                                                          |
+    //              +--------------------------------------------------------------------------+
+
+
     private void navigateToLoginActivity() {
         Intent intent = new Intent(this, LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
     }
+
+
+    //              +--------------------------------------------------------------------------+
+    //              |                                                                          |
+    //              |                     APERTURA Y GESTIÓN DE FRAGMENTOS                     |
+    //              |                                                                          |
+    //              +--------------------------------------------------------------------------+
+
+    private void openMapFragment()
+    {
+        actualFragment = FragmentType.MAP;
+        showToast("Mapa - Por implementar");
+    }
+
+    private void openProfileFragment()
+    {
+        actualFragment = FragmentType.PROFILE;
+        showToast("Perfil - Por implementar");
+    }
+
+    private void openSettingsFragment()
+    {
+        actualFragment = FragmentType.SETTINGS;
+        showToast("Ajustes - Por implementar");
+    }
+
+
+    //              +--------------------------------------------------------------------------+
+    //              |                                                                          |
+    //              |               IMPLEMENTACION DE LISTENERS DE FRAGMENTOS                  |
+    //              |                                                                          |
+    //              +--------------------------------------------------------------------------+
+
+
+    // ----- LISTENER MAP
+
+
+    // ----- LISTENER PROFILE
+
+
+    // ----- LISTENER SETTINGS
+
+
+
+
+
+
+
+
 }
