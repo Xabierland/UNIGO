476f9938bc7dc19c8d210225d61e372b774bd813
Eneko Etxaniz Monge <eenneekkooeemm@gmail.com>
2025-05-07 17:57:52 +0200

    Mapa de google implementado con posicion actual. La API esta en discord

--- src/app/src/main/java/com/ehunzango/unigo/fragments/MapFragment.java
+++ src/app/src/main/java/com/ehunzango/unigo/fragments/MapFragment.java
@@ -1,23 +1,39 @@
 package com.ehunzango.unigo.fragments;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.ehunzango.unigo.R;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment
 {
 
     private ListenerFragmentMap listener;
+
+    private GoogleMap mapGoogle;
+    private FusedLocationProviderClient locationClient;
+    private LatLng userPosition;
 
     public MapFragment()
     {
@@ -50,8 +66,79 @@ public class MapFragment extends Fragment
     {
         super.onViewCreated(view, savedInstanceState);
 
-        listener.askLocationPermission();
+
+        // Pedir permisos de ubicación
+        if (listener != null)
+        {
+            listener.askLocationPermission();
+        }
+
+        // Inicializar cliente de ubicación
+        locationClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        obtainActualLocation(false);
+
+        // Obtener el fragmento del mapa e inicializarlo
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment);
+
+        if (mapFragment != null)
+        {
+            mapFragment.getMapAsync(new OnMapReadyCallback()
+            {
+                @Override
+                public void onMapReady(@NonNull GoogleMap googleMap)
+                {
+                    mapGoogle = googleMap;
+
+                    // Ubicación de ejemplo: Bogotá
+                    //LatLng bogota = new LatLng(4.7110, -74.0721);
+                    //mapGoogle.addMarker(new MarkerOptions().position(bogota).title("Marker en Bogotá"));
+                    if(userPosition != null)
+                    {
+                        mapGoogle.addMarker(new MarkerOptions().position(userPosition).title("OWO"));
+                        mapGoogle.moveCamera(CameraUpdateFactory.newLatLngZoom(userPosition, 12f));
+                    }
+                    else
+                    {
+                        obtainActualLocation(true);
+                    }
+                }
+            });
+        }
     }
+
+
+
+    private void obtainActualLocation(boolean updateMap)
+    {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            //listener.salirDelMapa(); //TODO
+            Toast.makeText(getContext(), "[obtainActualLocation] NO PEWMISSION", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        locationClient  .getLastLocation()
+                        .addOnSuccessListener(getActivity(), location ->
+                        {
+                            userPosition = null;
+                            if (location != null) {
+                                double lat = location.getLatitude();
+                                double lon = location.getLongitude();
+
+                                userPosition = new LatLng(lat, lon);
+
+                                if(updateMap)
+                                {
+                                    mapGoogle.addMarker(new MarkerOptions().position(userPosition).title("OWO"));
+                                    mapGoogle.moveCamera(CameraUpdateFactory.newLatLngZoom(userPosition, 12f));
+                                }
+                            }
+
+                        });
+    }
+
 
     //              +--------------------------------------------------------------------------+
     //              |                                                                          |
