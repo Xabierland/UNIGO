7d0a73ff935873b9d9dfd9a57f4b1ac96463f01c
Xabier Gabiña <xabierland@gmail.com>
2025-05-18 21:20:59 +0200

    Color fijo en el dropdownloist

--- src/app/src/main/java/com/ehunzango/unigo/adapters/ImageSpinnerAdapter.java
+++ src/app/src/main/java/com/ehunzango/unigo/adapters/ImageSpinnerAdapter.java
@@ -1,6 +1,7 @@
 package com.ehunzango.unigo.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,31 +23,29 @@ public class ImageSpinnerAdapter  extends ArrayAdapter<SpinnerImageItem>
         this.items = items;
     }
 
-    private View getCustomView(int position, View convertView, ViewGroup parent)
-    {
-        if (convertView == null)
-        {
+    private View getCustomView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.transport_spinner_item, parent, false);
         }
 
         ImageView icon = convertView.findViewById(R.id.icon);
-
         SpinnerImageItem item = items.get(position);
         icon.setImageResource(item.iconResId);
+
+        // Fijar un color constante para los iconos
+        icon.setColorFilter(Color.BLACK);
 
         return convertView;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
-        return getCustomView(position, convertView, parent); // Selected view
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return getCustomView(position, convertView, parent); // Vista seleccionada
     }
 
     @Override
-    public View getDropDownView(int position, View convertView, ViewGroup parent)
-    {
-        return getCustomView(position, convertView, parent); // Dropdown list
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return getCustomView(position, convertView, parent); // Lista desplegable
     }
 }
 
